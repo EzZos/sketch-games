9882a36b395bb86cfa5e5712a892c05f8f035a0e
João Zumaêta <ezzoscode@gmail.com>
2023-04-17 15:22:24 -0300

    feat: add className optional props now the component can recive aditional styles if necessary

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,9 @@ import { ReactNode } from "react";
 
 interface ContainerProps {
   children: ReactNode;
+  className?: string;
 }
-export function Container({ children }: ContainerProps) {
-  return <div className="m-auto w-[1150px] max-w-[1150px]">{children}</div>;
+
+export function Container({ children, className }: ContainerProps) {
+  return <div className={`m-auto w-[1150px] max-w-[1150px] ${className}`}>{children}</div>;
 }
